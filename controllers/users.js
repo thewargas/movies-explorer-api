c104d1a0f228c8bf7657aab0c9b877475ed488c3
Vasiliy <thewargas@mail.ru>
2023-07-27 20:09:42 +0300

    fix: changeUserInfo controller

--- controllers/users.js
+++ controllers/users.js
@@ -64,20 +64,13 @@ const getUser = (req, res, next) => {
 const changeUserInfo = (req, res, next) => {
   const { _id } = req.user;
   const { name, email } = req.body;
-  User.findOne({ email: `${email}` })
-    .then((userWithMail) => {
-      if (!userWithMail) {
-        User.findByIdAndUpdate(_id, { name, email }, { new: true, runValidators: true })
-          .then((user) => {
-            if (!user) {
-              throw new NotFoundError('По данному _id информация не найдена');
-            }
-            res.send(user);
-          })
-          .catch(next);
-      } else {
-        throw new ConflictError('Пoльзователь с таким email уже зарегистрирован');
+
+  User.findByIdAndUpdate(_id, { name, email }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('По данному _id информация не найдена');
       }
+      res.send(user);
     })
     .catch(next);
 };
